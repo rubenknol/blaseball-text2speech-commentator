01bd137dd5e7d91e64246b647407a0b767de3211
Ruben Beltran del Rio <ben@nsovocal.com>
2020-08-25 19:53:04 +0200

    Add missing semicolons

--- say.js
+++ say.js
@@ -3,11 +3,11 @@ const Exec = promisify(require('child_process').exec);
 
 const internals = {
   kCommand: 'say'
-}
+};
 
 // Takes a message as a string. Says it out loud.
 
 module.exports = async function say(message='') {
 
   await Exec(`${internals.kCommand} '${message}'`);
-}
+};
